e1442c33586f6d24cd7f900ba448952d9129b559
BlueIceFrog <36274726+blueicefrog@users.noreply.github.com>
2021-05-15 12:43:31 +0000

    Added template code to LidarSensor

--- Sensors/LidarSensor.gd
+++ Sensors/LidarSensor.gd
@@ -1,16 +1,24 @@
-extends RigidBody
+extends "BaseSensor.gd"
 
+export var lidar_resolution = Vector2(400,500)
+var rotation_speed = 5.0 # The angular speed (degrees/s)
+var angle_range = 45.0 #The maximum deviation from forward dir that will get readings (degrees)
+var poll_rate = 3.0 #distance reads per second (Hz)
+var noise = 1.5 # standard deviation of distance read noise- (make % based?)
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$LidarBody/Viewport.size=lidar_resolution
+	
+
+func _process(delta):
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _render_view():
+	#return <TEXTURE>
+	pass
+	
+
+
+
+
